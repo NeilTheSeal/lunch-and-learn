d193a336425d54b4e2a492c11a7bc5d8ad3e82a5
NeilTheSeal <neil.hendren@gmail.com>
2024-06-11 16:41:08 -0600

    Remove unnecessary code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,26 +20,4 @@ class Api::V1::UsersController < ApplicationController
              status: :bad_request
     end
   end
-
-  def login
-    user = User.find_by(username: params[:username])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.username}"
-      if user.admin?
-        redirect_to admin_dashboard_path
-      else
-        redirect_to root_path
-      end
-    else
-      flash[:error] = "Sorry, your credentials are bad."
-      render :login_form
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :password)
-  end
 end
