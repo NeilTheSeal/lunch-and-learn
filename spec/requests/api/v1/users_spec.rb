84934b5c9a9139d47c0b7cb40f4c99cb6f3d7325
NeilTheSeal <neil.hendren@gmail.com>
2024-06-11 16:38:55 -0600

    Write tests for user login

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -88,4 +88,101 @@ RSpec.describe "Users API", type: :request do
       expect(body[:errors]).to eq("Password confirmation doesn't match Password")
     end
   end
+
+  describe "POST /api/v1/sessions" do
+    it "happy path - logs in a user" do
+      user = User.create(
+        name: "test",
+        email: "test@test.com",
+        password: "password",
+        api_key: SecureRandom.hex(13)
+      )
+
+      post(
+        "/api/v1/sessions",
+        params: {
+          email: "test@test.com",
+          password: "password"
+        }.to_json,
+        headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        }
+      )
+
+      expect(response).to have_http_status(201)
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body).to have_key(:data)
+      expect(body[:data]).to have_key(:id)
+      expect(body[:data][:type]).to eq("user")
+      expect(body[:data][:attributes]).to be_a(Hash)
+
+      expect(body[:data][:attributes]).to have_key(:name)
+      expect(body[:data][:attributes][:name]).to be_a(String)
+
+      expect(body[:data][:attributes]).to have_key(:email)
+      expect(body[:data][:attributes][:email]).to be_a(String)
+
+      expect(body[:data][:attributes]).to have_key(:api_key)
+      expect(body[:data][:attributes][:api_key]).to be_a(String)
+    end
+
+    it "sad path - bad credentials (bad email)" do
+      user = User.create(
+        name: "test",
+        email: "test@test.com",
+        password: "password",
+        api_key: SecureRandom.hex(13)
+      )
+
+      post(
+        "/api/v1/sessions",
+        params: {
+          email: "wrong@test.com",
+          password: "password"
+        }.to_json,
+        headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        }
+      )
+
+      expect(response).to have_http_status(400)
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body).to have_key(:errors)
+      expect(body[:errors]).to eq("Sorry, your credentials are bad.")
+    end
+
+    it "sad path - bad credentials (bad password)" do
+      user = User.create(
+        name: "test",
+        email: "test@test.com",
+        password: "password",
+        api_key: SecureRandom.hex(13)
+      )
+
+      post(
+        "/api/v1/sessions",
+        params: {
+          email: "test@test.com",
+          password: "wrong_password"
+        }.to_json,
+        headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        }
+      )
+
+      expect(response).to have_http_status(400)
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body).to have_key(:errors)
+      expect(body[:errors]).to eq("Sorry, your credentials are bad.")
+    end
+  end
 end
